651b7726d883f3dd0dae501b9f648654181126a5
MrPotatsus <MrPotatsus@yandex.ru>
2023-10-02 10:11:24 +0300

    Restructured bot architecture

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -1,10 +1,13 @@
-from collections import defaultdict
-
+# Third-party
 from aiogram.types import *
 
-from utils.str_resources import *
-from data.database.models import Order
-from init_loader import dp, db, cf, bot, logger
+# Project
+import strings
+import config as cf
+from database.models import Order
+from commands import handle_choose_product
+from start import *
+
 
 async def __process_callback_data(callback_data) -> str | tuple:
     """
@@ -18,7 +21,7 @@ async def __process_callback_data(callback_data) -> str | tuple:
         return tuple(result)
 
 
-@dp.callback_query_handler(lambda query: 'category_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'category_data' in query.data)
 async def handle_category_callback(callback: CallbackQuery):
     """
     Callback handler for category selection.
@@ -30,15 +33,14 @@ async def handle_category_callback(callback: CallbackQuery):
     await callback.message.delete()
 
     category = await __process_callback_data(callback.data)
-    for product in await db.get_products_by_category(category):
+    for product in await database.product.get_by_category(category):
         # Retrieve message and reply markup for the product card
-        msg, keyboard = await get_product_card_info_msg(
+        msg, keyboard = await strings.Ru.get_product_card_info_msg(
             user_id=callback.from_user.id,
             product=product
         )
-
         # Send the product card photo message
-        message = await bot.send_photo(
+        await bot.send_photo(
             chat_id=callback.message.chat.id,
             photo=product.image_url,
             caption=msg,
@@ -46,7 +48,7 @@ async def handle_category_callback(callback: CallbackQuery):
         )
 
     # Send a message with an option to go back to the catalog
-    msg, keyboard = await back_to_catalogue_msg()
+    msg, keyboard = strings.Ru.back_to_catalogue_msg()
     await bot.send_message(
         chat_id=callback.message.chat.id,
         text=msg,
@@ -54,13 +56,12 @@ async def handle_category_callback(callback: CallbackQuery):
     )
 
 
-@dp.callback_query_handler(lambda query: 'back_to_catalogue_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'back_to_catalogue_data' in query.data)
 async def handle_back_to_catalogue_callback(callback: CallbackQuery):
     """
     Callback handler for going back to catalogue.
     Deletes previously sent messages and invokes the handler to choose a product.
     """
-    from chat.handlers.chat_handler import handle_choose_product
     await logger.info('Handling back to categories callback')
     await bot.answer_callback_query(callback.id)
 
@@ -70,15 +71,15 @@ async def handle_back_to_catalogue_callback(callback: CallbackQuery):
     await handle_choose_product(callback.message)
 
 
-@dp.callback_query_handler(lambda query: 'add_to_cart_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'add_to_cart_data' in query.data)
 async def handle_add_to_cart_callback(callback: CallbackQuery):
     """
     Callback handler for adding a product to the cart.
     """
     await logger.info('Handling add to cart data callback')
     product_id = int(await __process_callback_data(callback.data))
-    product = await db.get_product_by_id(product_id)
-    user = await db.get_user_by_id(callback.from_user.id)
+    product = await database.product.get_by_id(product_id)
+    user = await database.user.get_by_id(callback.from_user.id)
 
     # Add the product to the user's cart
     if product.category not in user.cart:
@@ -89,15 +90,15 @@ async def handle_add_to_cart_callback(callback: CallbackQuery):
             'product_id': product.product_id, 'name': product.name,
             'amount': 1, 'total_cost': product.cost * ((100 - product.discount) / 100)
         })
-    await db.update_user(user)
+    await database.user.update(user)
 
     # Update product cards information
     await bot.answer_callback_query(callback.id)
-    msg, keyboard = await get_product_card_info_msg(user.user_id, product)
+    msg, keyboard = await strings.Ru.get_product_card_info_msg(user.user_id, product)
     await callback.message.edit_caption(caption=msg, reply_markup=keyboard)
 
 
-@dp.callback_query_handler(lambda query: 'remove_from_cart_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'remove_from_cart_data' in query.data)
 async def handle_remove_from_cart_callback(callback: CallbackQuery):
     """
     Callback handler for removing a product from the cart.
@@ -105,7 +106,7 @@ async def handle_remove_from_cart_callback(callback: CallbackQuery):
     await logger.info('Handling remove from cart callback')
     product_id, in_cart = await __process_callback_data(callback.data)
     product_id, in_cart = int(product_id), True if in_cart == 'True' else False
-    product, user = await db.get_product_by_id(product_id), await db.get_user_by_id(callback.from_user.id)
+    product, user = await database.product.get_by_id(product_id), await database.user.get_by_id(callback.from_user.id)
 
     # Remove the product from the user's cart
     for i, product_info in enumerate(user.cart[product.category]):
@@ -117,16 +118,16 @@ async def handle_remove_from_cart_callback(callback: CallbackQuery):
     if not user.cart[product.category]:
         del user.cart[product.category]
 
-    await db.update_user(user)
+    await database.user.update(user)
     await bot.answer_callback_query(callback.id)
     if in_cart:
         await callback.message.delete()
     else:
-        msg, keyboard = await get_product_card_info_msg(user.user_id, product)
+        msg, keyboard = await strings.Ru.get_product_card_info_msg(user.user_id, product)
         await callback.message.edit_caption(caption=msg, reply_markup=keyboard)
 
 
-@dp.callback_query_handler(lambda query: 'change_amount_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'change_amount_data' in query.data)
 async def handle_change_amount_callback(callback: CallbackQuery):
     """
     Callback handler for changing the amount of a product in the cart.
@@ -136,8 +137,8 @@ async def handle_change_amount_callback(callback: CallbackQuery):
     product_id, category, cart_amount = int(product_id), category, int(cart_amount)
     is_adding = True if is_adding == 'True' else False
 
-    product = await db.get_product_by_id(product_id)
-    user = await db.get_user_by_id(callback.from_user.id)
+    product = await database.product.get_by_id(product_id)
+    user = await database.user.get_by_id(callback.from_user.id)
 
     # Update the cart amount based on the is_adding flag
     if is_adding and product.amount > cart_amount:
@@ -151,14 +152,14 @@ async def handle_change_amount_callback(callback: CallbackQuery):
         if product_info['product_id'] == product_id:
             product_info['amount'] = cart_amount
             product_info['total_cost'] = total_cost
-    await db.update_user(user)
+    await database.user.update(user)
 
     await bot.answer_callback_query(callback.id)
-    msg, keyboard = await update_cart_msg(callback.message.text, cart_amount, total_cost, product)
+    msg, keyboard = await strings.Ru.update_cart_msg(callback.message.text, cart_amount, total_cost, product)
     await callback.message.edit_text(msg, reply_markup=keyboard)
 
 
-@dp.callback_query_handler(lambda query: 'payment_data' in query.data)
+@bot.dispatcher.callback_query_handler(lambda query: 'payment_data' in query.data)
 async def handle_payment_callback(callback: CallbackQuery):
     """
     Callback handler for processing payment.
@@ -170,22 +171,21 @@ async def handle_payment_callback(callback: CallbackQuery):
     provider = await __process_callback_data(callback.data)
     match provider:
         case 'Sberbank':
-            provider_token = cf.SBERBANK
+            provider_token = cf.payment['sber']
         case 'UKassa':
-            provider_token = cf.UKASSA
+            provider_token = cf.payment['ukassa']
         case _:
             provider_token = ''
 
     # Prepare the prices for the invoice
-    user = await db.get_user_by_id(callback.from_user.id)
+    user = await database.user.get_by_id(callback.from_user.id)
     prices = []
     for cat, cat_products in user.cart.items():
         for product in cat_products:
             prices.append(LabeledPrice(f'{product["name"]} x{product["amount"]}', int(product["total_cost"] * 100)))
 
     # Send the invoice to the user
-    await logging
-    title, description = await invoice_msg()
+    title, description = strings.Ru.invoice_msg()
     if prices:
         await bot.send_invoice(
             chat_id=callback.message.chat.id,
@@ -199,9 +199,10 @@ async def handle_payment_callback(callback: CallbackQuery):
             protect_content=True,
         )
     else:
-        await callback.message.answer(await nothing_to_pay())
+        await callback.message.answer(strings.Ru.nothing_to_pay())
 
-@dp.pre_checkout_query_handler()
+
+@bot.dispatcher.pre_checkout_query_handler()
 async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery):
     """
     Handler for processing pre-checkout queries.
@@ -210,7 +211,7 @@ async def process_pre_checkout_query(pre_checkout_query: PreCheckoutQuery):
     await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 
 
-@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+@bot.dispatcher.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
 async def successful_payment(message: Message):
     """
     Handler for successful payment.
@@ -232,8 +233,8 @@ async def successful_payment(message: Message):
 
     # Insert the order into the database
     order_id = str(uuid4())
-    user = await db.get_user_by_id(message.from_user.id)
-    await db.insert_order(Order(
+    user = await database.user.get_by_id(message.from_user.id)
+    await database.order.insert(Order(
         order_id=order_id,
         products_info=user.cart,
         address=address,
@@ -243,17 +244,17 @@ async def successful_payment(message: Message):
     # Update the product quantities
     for products in user.cart.values():
         for product_info in products:
-            product = await db.get_product_by_id(product_info['product_id'])
+            product = await database.product.get_by_id(product_info['product_id'])
             product.amount -= product_info['amount']
-            await db.update_product(product)
+            await database.product.update(product)
 
     # Clear the user's cart and update the user in the database
     user.cart = {}
     user.order_ids.append(order_id)
-    await db.update_user(user)
+    await database.user.update(user)
 
     # Send the success message
-    await bot.send_message(message.chat.id, await successful_payment_msg(
+    await bot.send_message(message.chat.id, strings.Ru.successful_payment_msg(
         total_price=message.successful_payment.total_amount // 100,
         currency=message.successful_payment.currency
     ))
--- bot/commands.py
+++ bot/commands.py
@@ -1,25 +1,25 @@
+# Third-party
 from aiogram.types import *
 
-from init_loader import *
-from utils.str_resources import *
-from data.database.models import User
-from chat.keyboards.reply_keyboard import menu_keyboard
+# Project
+import keyboards as kb
+import strings
+from database.models import User
+from start import *
 
 
-@dp.message_handler(commands=['start'])
-@dp.throttled(anti_spam, rate=cf.SPAM_RATE)
+@bot.dispatcher.message_handler(commands=['start'])
 async def start(message: Message):
     """
     Handler for the '/start' command.
     Sends a greeting message and inserts the user into the database if not already present.
     """
     await logger.info('Start Command')
-    await message.answer(await greeting_msg(), reply_markup=menu_keyboard)
-    await db.insert_user_if_not_exist(User().init_values(message.from_user.id))
+    await message.answer(strings.Ru.greeting_msg(), reply_markup=kb.menu_keyboard)
+    await database.user.insert_if_not_exist(User().init_values(message.from_user.id))
 
 
-@dp.message_handler(content_types=['text'])
-@dp.throttled(anti_spam, rate=cf.SPAM_RATE)
+@bot.dispatcher.message_handler(content_types=['text'])
 async def menu_keyboard_handler(message: Message):
     """
     Handler for processing text messages based on the menu keyboard options.
@@ -28,15 +28,15 @@ async def menu_keyboard_handler(message: Message):
     await logger.info(f'Command - {message.text}')
 
     # Check the message text and execute corresponding actions
-    if message.text == about_name():
-        await message.answer(await about_msg())
-    elif message.text == faq_name():
-        await message.answer(await faq_msg())
-    elif message.text == cart_name():
+    if message.text == strings.Ru.about_name():
+        await message.answer(strings.Ru.about_msg())
+    elif message.text == strings.Ru.faq_name():
+        await message.answer(strings.Ru.faq_msg())
+    elif message.text == strings.Ru.cart_name():
         await handle_cart(message)
-    elif message.text == choose_product_name():
+    elif message.text == strings.Ru.choose_product_name():
         await handle_choose_product(message)
-    elif message.text == payment_name():
+    elif message.text == strings.Ru.payment_name():
         await handle_payment(message)
 
 
@@ -45,16 +45,17 @@ async def handle_cart(message: Message):
     Handler for processing cart messages.
     Retrieves the user's cart from the database and sends cart information or empty cart message.
     """
-    user = await db.get_user_by_id(message.from_user.id)
+    user = await database.user.get_by_id(user_id=message.from_user.id)
     if user.cart:
-        for category in await db.get_categories():
+        for category in await database.product.fetch_categories():
             if category in user.cart:
                 for product_info in user.cart[category]:
-                    product = await db.get_product_by_id(product_info['product_id'])
-                    msg, keyboard = await cart_msg(category, product_info, product)
+                    product = await database.product.get_by_id(product_id=product_info['product_id'])
+                    msg, keyboard = await strings.Ru.cart_msg(category=category, product_info=product_info,
+                                                              product=product)
                     await message.answer(msg, reply_markup=keyboard)
     else:
-        await message.answer(await empty_cart_msg())
+        await message.answer(strings.Ru.empty_cart_msg())
 
 
 async def handle_payment(message: Message):
@@ -62,7 +63,7 @@ async def handle_payment(message: Message):
     Handler for processing payment messages.
     Sends a message with payment options.
     """
-    msg, keyboard = await choose_payment_msg()
+    msg, keyboard = strings.Ru.choose_payment_msg(), await kb.get_payment_keyboard()
     await message.answer(msg, reply_markup=keyboard)
 
 
@@ -71,9 +72,9 @@ async def handle_choose_product(message: Message):
     Handler for processing product selection messages.
     Sends a message with category options or empty catalogue message if no categories are available.
     """
-    categories = await db.get_categories()
+    categories = await database.product.fetch_categories()
     if categories:
-        msg, keyboard = await choose_category_msg(categories)
+        msg, keyboard = strings.Ru.choose_category_msg(), await kb.get_categories_keyboard(categories)
         await message.answer(text=msg, reply_markup=keyboard)
     else:
-        await message.answer(await empty_catalogue_msg())
+        await message.answer(strings.Ru.empty_cart_msg())
